6ce43d7be302df4735b460a807f946870c2a9f34
Gonzalo Benitez <gonzalorubenbenitez@gmail.com>
2011-08-31 11:04:49 -0300

    app created

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ Ext.application({
     name: 'Example',
     appFolder: 'app',
     launch: function() {
-    	
+    	Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: [
+                {
+                    xtype: 'panel',
+                    title: 'Panel Example',
+                    html : 'Grid will go here'
+                }
+            ]
+        });
          });
     }
 });
